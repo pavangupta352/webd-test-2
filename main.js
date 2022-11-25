c8d481677c944bf5613badf2fa36e52f54477db4
Pavan Gupta <77353814+pavangupta352@users.noreply.github.com>
2022-11-25 22:43:54 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+function change(ref) {
+    console.log(ref)
+
+    document.getElementById('radioo').style.backgroundColor = ref;
+    if (ref === 'black') {
+        document.getElementById('search').style.color = 'white'
+    }
+}
+function load() {
+
+
+    const x = document.getElementById("srch")
+    fetch('https://api.tvmaze.com/search/shows?q=' + x.value).then(res => res.json()).then(
+        res => {
+            console.log(res[0].show.image.medium)
+            var t = "";
+            res.map((r) => {
+                if (r.show.image != null)
+                    t += '<img src="' + r.show.image.medium + '" alt="" width="260" height="320">'
+            })
+            document.getElementById('app').innerHTML = t;
+
+        }
+    )
+}
